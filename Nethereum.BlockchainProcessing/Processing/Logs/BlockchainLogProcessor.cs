8abbe0d1dc3ced1b05d8ebccbce2802a91c18d94
Dave Whiffin <dave.whiffin@gmail.com>
2018-11-19 15:21:05 +0000

    Changing long to ulong for block number

---
+++ Nethereum.BlockchainProcessing/Processing/Logs/BlockchainLogProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nethereum.BlockchainProcessing.Web3Abstractions;
+using Nethereum.RPC.Eth.DTOs;
+
+namespace Nethereum.BlockchainProcessing.Processing.Logs
+{
+    public class BlockchainEventLogProcessor : IBlockchainProcessor
+    {
+        private readonly IEventLogProxy _eventLogProxy;
+        private readonly IEnumerable<IEventLogProcessor> _logProcessors;
+
+        public BlockchainEventLogProcessor(IEventLogProxy eventLogProxy, IEnumerable<IEventLogProcessor> logProcessors)
+        {
+            _eventLogProxy = eventLogProxy;
+            _logProcessors = logProcessors;
+        }
+
+        public async Task ProcessAsync(ulong fromBlockNumber, ulong toBlockNumber)
+        {
+            await ProcessAsync(fromBlockNumber, toBlockNumber, new CancellationToken());
+        }
+
+        public async Task ProcessAsync(ulong fromBlockNumber, ulong toBlockNumber, CancellationToken cancellationToken)
+        {
+            var logs = await _eventLogProxy.GetLogs(new NewFilterInput
+            {
+                FromBlock = new BlockParameter(fromBlockNumber),
+                ToBlock = new BlockParameter(toBlockNumber)
+            });
+
+            if (logs == null) return;
+
+            var processingCollection = new List<LogsMatchedForProcessing>();
+
+            foreach (var logProcessor in _logProcessors)
+            {
+                processingCollection.Add(new LogsMatchedForProcessing(logProcessor));
+            }
+
+            foreach (var log in logs)
+            {
+                foreach (var matchedForProcessing in processingCollection)
+                {
+                    matchedForProcessing.AddIfMatched(log);
+                }
+            }
+
+            foreach (var matchedForProcessing in processingCollection)
+            {
+                await matchedForProcessing.ProcessLogsAsync();
+            }
+        }
+    }
+}
---
+++ Nethereum.BlockchainProcessing/Processing/Logs/LogsMatchedForProcessing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.RPC.Eth.DTOs;
+
+namespace Nethereum.BlockchainProcessing.Processing.Logs
+{
+    public class LogsMatchedForProcessing
+    {
+        public LogsMatchedForProcessing(IEventLogProcessor logProcessor)
+        {
+            LogProcessor = logProcessor;
+            MatchedLogs = new List<FilterLog>();
+        }
+
+        public IEventLogProcessor LogProcessor { get; }
+        public List<FilterLog> MatchedLogs { get; }
+
+        public void AddIfMatched(FilterLog log)
+        {
+            if (LogProcessor.IsLogForEvent(log))
+            {
+                MatchedLogs.Add(log);
+            }
+        }
+
+        public async Task ProcessLogsAsync()
+        {
+            await LogProcessor.ProcessLogsAsync(this.MatchedLogs.ToArray());
+        }
+    }
+}
