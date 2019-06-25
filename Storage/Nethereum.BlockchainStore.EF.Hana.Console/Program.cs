47e19794cd99c1a9681f946b36440b1e3b389004
Kevin Small <kevin.small@consensys.net>
2019-06-25 10:43:34 +0100

    Adding HANA console

---
+++ Storage/Nethereum.BlockchainStore.EF.Hana.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.BlockchainStore.EF;
+using Nethereum.BlockchainStore.EF.Hana;
+using Nethereum.BlockchainStore.Processing;
+using Nethereum.BlockchainProcessing.Processing;
+using Microsoft.Configuration.Utils;
+using Microsoft.Logging.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace Nethereum.BlockchainStore.EF.Hana.Console
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var log = ApplicationLogging.CreateConsoleLogger<Program>().ToILog();
+
+            var appConfig = ConfigurationUtils
+                .Build(args, userSecretsId: "PurchaseOrderEthereumToSap");
+
+            var configuration = BlockchainSourceConfigurationFactory.Get(appConfig);
+            var hanaDbContextFactory = new HanaBlockchainDbContextFactory("BlockchainDbContext_hana", "DEMO");
+            var repositoryFactory = new BlockchainStoreRepositoryFactory(hanaDbContextFactory);
+            return StorageProcessorConsole.Execute(repositoryFactory, configuration, log: log).Result;
+        }
+    }
+}
--- Storage/Nethereum.BlockchainStore.EF.Hana.Tests/README.md
+++ Storage/Nethereum.BlockchainStore.EF.Hana.Tests/README.md
@@ -7,7 +7,8 @@ To build for HANA:
 
 1. Install HANA data provider on the same PC that build will happen. Instructions here: https://help.sap.com/viewer/1efad1691c1f496b8b580064a6536c2d/Cloud/en-US/7017cce72a054111b71cc713762e365c.html
 2. Rename  `App.config.EXAMPLE` to be `App.config`.
-3. Build project `Nethereum.BlockchainStore.EF.Hana.Tests`.
+3. Edit `App.config` file to have the correct connection string for your HANA system.
+4. Build project `Nethereum.BlockchainStore.EF.Hana.Tests`.
 
 The build should complete successfully.
 
@@ -16,9 +17,7 @@ The build should complete successfully.
 
 1. A HANA 2.x system is required.
 2. Ensure that the schemas `dbo` and `DEMO` exist on the HANA system. (Edit `TestHanaContextFactory.cs` to write blockchain data to a schema other than `DEMO`, but `dbo` is always required for EF).
-3. Edit the `App.config` file to have the correct connection string.
-
-The tests should complete successfully.
+3. The tests in test folder `Nethereum.BlockchainStore.EF.Hana.Tests` should complete successfully.
 
 
 
